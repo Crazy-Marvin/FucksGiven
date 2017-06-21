021e47df21d7d916d6d26ce4288800df42751022
Fenil <fenilshah1010@gmail.com>
2017-06-22 01:03:27 +0530

    managing dependencies at one place from now

--- mobile/src/main/java/rocks/poopjournal/fucksgiven/MainActivity.java
+++ mobile/src/main/java/rocks/poopjournal/fucksgiven/MainActivity.java
@@ -1,9 +1,10 @@
 package rocks.poopjournal.fucksgiven;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
