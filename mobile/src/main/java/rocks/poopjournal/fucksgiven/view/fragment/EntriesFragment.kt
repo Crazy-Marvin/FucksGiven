3385a4cfc6fbb045c464e1b0f9ee0e396bd738da
Fenil <fenilshah1010@gmail.com>
2017-06-24 15:53:40 +0530

    added values

--- mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/EntriesFragment.kt
+++ mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/EntriesFragment.kt
@@ -8,7 +8,6 @@ import rocks.poopjournal.fucksgiven.view.adapter.EntriesAdapter
 
 class EntriesFragment : BaseFragment() {
 
-
     private var adapter: EntriesAdapter? = null
 
     override fun provideLayout(): Int {
