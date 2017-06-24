a98f10e68389a0a06d4d68a5aba07d25e0dcd0e4
Fenil <fenilshah1010@gmail.com>
2017-06-24 15:11:38 +0530

    added adapter for entries

--- mobile/src/main/java/rocks/poopjournal/fucksgiven/view/adapter/EntriesAdapter.kt
+++ mobile/src/main/java/rocks/poopjournal/fucksgiven/view/adapter/EntriesAdapter.kt
@@ -1,4 +1,30 @@
 package rocks.poopjournal.fucksgiven.view.adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import rocks.poopjournal.fucksgiven.R
+import rocks.poopjournal.fucksgiven.model.Entry
 
-class EntriesAdapter
+
+class EntriesAdapter(context: Context, models: List<Entry>) : BaseRecyclerAdapter<Entry, EntriesAdapter.EntryHolder>(context, models) {
+
+    override fun provideLayout(): Int {
+        return R.layout.item_entries
+    }
+
+    override fun generateViewHolder(view: View): EntryHolder {
+        return EntryHolder(view)
+    }
+
+    override fun bind(holder: EntryHolder, model: Entry) {
+
+    }
+
+
+    class EntryHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        init {
+
+        }
+    }
+}
--- mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/EntriesFragment.kt
+++ mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/EntriesFragment.kt
@@ -1,16 +1,21 @@
 package rocks.poopjournal.fucksgiven.view.fragment
 
+import kotlinx.android.synthetic.main.fragmet_entries.*
 import rocks.poopjournal.fucksgiven.R
+import rocks.poopjournal.fucksgiven.model.Entry
+import rocks.poopjournal.fucksgiven.view.adapter.EntriesAdapter
 
 
 class EntriesFragment : BaseFragment() {
+
+    private val entries = listOf<Entry>()
 
     override fun provideLayout(): Int {
         return R.layout.fragmet_entries
     }
 
     override fun init() {
-
+        rvEntries.adapter = EntriesAdapter(context, entries)
     }
 
 }
