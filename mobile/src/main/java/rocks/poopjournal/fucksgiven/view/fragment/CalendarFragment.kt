1525f1bbf2b1701bac0dc7f6b8dc18a3265ccebd
Harsh <bhavsarharshj@gmail.com>
2017-06-30 23:47:27 +0530

    added calendar library

--- mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/CalendarFragment.kt
+++ mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/CalendarFragment.kt
@@ -1,9 +1,14 @@
 package rocks.poopjournal.fucksgiven.view.fragment
 
+import com.github.gfranks.collapsible.calendar.CollapsibleCalendarView
+import com.github.gfranks.collapsible.calendar.model.CollapsibleCalendarEvent
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import org.joda.time.LocalDate
 import rocks.poopjournal.fucksgiven.R
 
 
-class CalendarFragment : BaseFragment() {
+class CalendarFragment : BaseFragment(), CollapsibleCalendarView.Listener<CollapsibleCalendarEvent> {
+
 
     override fun provideLayout(): Int {
         return R.layout.fragment_calendar
@@ -11,7 +16,19 @@ class CalendarFragment : BaseFragment() {
 
     override fun init() {
 
+        collapsibleCalendar.setListener(this)
+
     }
 
+    override fun onDateSelected(date: LocalDate?, events: MutableList<CollapsibleCalendarEvent>?) {
 
+    }
+
+    override fun onMonthChanged(date: LocalDate?) {
+
+    }
+
+    override fun onHeaderClick() {
+
+    }
 }
