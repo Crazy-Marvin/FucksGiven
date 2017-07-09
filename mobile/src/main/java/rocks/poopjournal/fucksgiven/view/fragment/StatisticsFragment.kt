e5569771b634dcf0320dc187254e0af864e46cc9
f1010 <fenilshah1010@gmail.com>
2017-07-09 17:09:29 +0530

    added charts

--- mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/StatisticsFragment.kt
+++ mobile/src/main/java/rocks/poopjournal/fucksgiven/view/fragment/StatisticsFragment.kt
@@ -1,21 +1,83 @@
 package rocks.poopjournal.fucksgiven.view.fragment
 
-import android.content.Intent
-import android.view.View
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.android.synthetic.main.fragment_statiscs.*
 import rocks.poopjournal.fucksgiven.R
-import rocks.poopjournal.fucksgiven.view.activity.AddFucksActivity
 
 
-class StatisticsFragment : BaseFragment(), View.OnClickListener {
-    override fun onClick(p0: View?) {
-        startActivity(Intent(activity, AddFucksActivity::class.java))
-    }
+class StatisticsFragment : BaseFragment() {
 
     override fun provideLayout(): Int {
         return R.layout.fragment_statiscs
     }
 
     override fun init() {
-            fabAddAction.setOnClickListener(this)
+        val data = BarData(getXAxisValues(), getDataSet())
+
+        chart.data = data
+        chart.setDescription(null)
+        chart.animateXY(2000, 2000)
+        chart.invalidate()
+    }
+
+
+    private fun getDataSet(): ArrayList<BarDataSet> {
+        var dataSets: ArrayList<BarDataSet>? = null
+
+        val valueSet1 = ArrayList<BarEntry>()
+        val v1e1 = BarEntry(1f, 0) // Jan
+        valueSet1.add(v1e1)
+        val v1e2 = BarEntry(4f, 1) // Feb
+        valueSet1.add(v1e2)
+        val v1e3 = BarEntry(6f, 2) // Mar
+        valueSet1.add(v1e3)
+        val v1e4 = BarEntry(3f, 3) // Apr
+        valueSet1.add(v1e4)
+        val v1e5 = BarEntry(9f, 4) // May
+        valueSet1.add(v1e5)
+        val v1e6 = BarEntry(1f, 5) // Jun
+        valueSet1.add(v1e6)
+        val v1e7 = BarEntry(4f, 6) // Jun
+        valueSet1.add(v1e7)
+
+        val valueSet2 = ArrayList<BarEntry>()
+        val v2e1 = BarEntry(5f, 0) // Jan
+        valueSet2.add(v2e1)
+        val v2e2 = BarEntry(9f, 1) // Feb
+        valueSet2.add(v2e2)
+        val v2e3 = BarEntry(1f, 2) // Mar
+        valueSet2.add(v2e3)
+        val v2e4 = BarEntry(6f, 3) // Apr
+        valueSet2.add(v2e4)
+        val v2e5 = BarEntry(2f, 4) // May
+        valueSet2.add(v2e5)
+        val v2e6 = BarEntry(7f, 5) // Jun
+        valueSet2.add(v2e6)
+        val v2e7 = BarEntry(5f, 6) // Jun
+        valueSet2.add(v2e7)
+        val barDataSet2 = BarDataSet(valueSet2, "Days")
+        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS)
+        barDataSet2.setValueFormatter { value ->
+            val formatted = value.toInt()
+            "$formatted Fucks"
+        }
+        dataSets = ArrayList()
+        dataSets.add(barDataSet2)
+        return dataSets
+    }
+
+    private fun getXAxisValues(): ArrayList<String> {
+        val xAxis = ArrayList<String>()
+        xAxis.add("SUN")
+        xAxis.add("MON")
+        xAxis.add("TUE")
+        xAxis.add("WED")
+        xAxis.add("THU")
+        xAxis.add("FRI")
+        xAxis.add("SAT")
+        return xAxis
     }
 }
