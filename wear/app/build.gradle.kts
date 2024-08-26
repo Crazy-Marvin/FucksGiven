03c05151d89b913a6f3dc2250986c683d3f0df79
CrazyMarvin <git@poopjournal.rocks>
2024-08-26 23:14:49 +0200

    Bump Version

--- wear/app/build.gradle.kts
+++ wear/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "rocks.poopjournal.fucksgivenwatch"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.4"
         vectorDrawables {
             useSupportLibrary = true
         }
