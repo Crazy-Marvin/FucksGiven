10005a672fd28ec301f0ca3e2d6976553e136c11
Marvin <marvin@poopjournal.rocks>
2024-08-26 21:47:34 +0200

    Update AGP

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "rocks.poopjournal.fucksgiven"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- wear/app/build.gradle.kts
+++ wear/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "rocks.poopjournal.fucksgivenwatch"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
