8f245163cc56cb71b1386e91597b5c29a5bd9590
Marvin <marvin@poopjournal.rocks>
2024-08-26 20:05:15 +0200

    Target Android 15

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 
 android {
     namespace = "rocks.poopjournal.fucksgiven"
-    compileSdk = 34
+    compileSdk = 35
 
 
     defaultConfig {
 
         applicationId = "rocks.poopjournal.fucksgiven"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
