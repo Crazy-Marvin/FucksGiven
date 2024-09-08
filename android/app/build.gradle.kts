6e2d59cbd71e329073266810102f0332c8af2a0a
CrazyMarvin <git@poopjournal.rocks>
2024-09-08 23:23:48 +0200

    Bump Version
    https://github.com/Crazy-Marvin/FucksGiven/milestone/1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "rocks.poopjournal.fucksgiven"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
