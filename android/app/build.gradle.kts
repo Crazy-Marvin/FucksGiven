64d40042525af181290a027b39a6bb38fc6e2c96
CrazyMarvin <git@poopjournal.rocks>
2026-03-26 22:32:39 +0100

    v1.2.0 -> v1.3.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "rocks.poopjournal.fucksgiven"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.2.0"
+        versionCode = 13
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
